14d4134ae9e8759c7bc77eb6ae9b2efbffca2d46
mibar <whitewhale@yandex.ru>
2022-03-14 13:13:17 +0300

    created Security Constans

--- src/main/java/com/tasksbb/train/security/SecurityConstans.java
+++ src/main/java/com/tasksbb/train/security/SecurityConstans.java
@@ -1,8 +1,12 @@
 package com.tasksbb.train.security;
 
 public class SecurityConstans {
-    public static final String SIGN_UP_URL ="/api/auth/**";
+    public static final String SIGN_UP_URL = "/api/auth/**";
+    public static final String TRAIN_URL = "/api/train/**";
+    public static final String STATION_URL = "/api/station/**";
+    public static final String ADMIN_URL = "/api/admin/**";
     public static final String SECRET = "SecretKeyGenJWT";
+
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String CONTENT_TYPE = "application/json";
