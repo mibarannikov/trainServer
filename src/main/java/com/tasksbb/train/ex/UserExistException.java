7b888b7d7fa7cab743ddb011d3691eb386312488
mibar <whitewhale@yandex.ru>
2022-03-14 13:04:18 +0300

    delete unused import

--- src/main/java/com/tasksbb/train/ex/UserExistException.java
+++ src/main/java/com/tasksbb/train/ex/UserExistException.java
@@ -1,7 +1,6 @@
 package com.tasksbb.train.ex;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
